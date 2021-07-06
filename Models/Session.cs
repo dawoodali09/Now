3f37b8b3eb0b17918963e5719e33abcfb4db3e36
Dawood Ali <dawoodali@gmail.com>
2021-07-06 22:58:40 +1200

    ...

---
+++ Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Models
+{
+
+    public class Session
+    {
+        public User account { get; set; }
+        public NameValueCollection headers { get; set; }
+        public Credentials credentials { get; set; }
+
+    }
+
+}
---
+++ MongoAccess/MongoData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoAccess
+{
+    public static class MongoData
+    {
+        public static void AddInstrumment(Instrument ins)
+        {
+            BsonDocument doc = ins.ToBsonDocument();
+            MongoClient dbClient = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+
+            var dbList = dbClient.ListDatabases().ToList();
+            var database = dbClient.GetDatabase("MarketData");
+            var collection = database.GetCollection<BsonDocument>("Instruments");
+
+            collection.InsertOne(doc);
+
+        }
+    }
+}
